fc67d040b4ad26438143fc463f4960a6429da6ec
Yakob Dereje <yakobdereje.yd@gmail.com>
2025-03-15 06:34:34 -0700

    students page done!

--- course/src/app/(dashboard)/teacher/page.tsx
+++ course/src/app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,28 @@
+import EventCalendar from "@/src/components/EventCalendar";
+import Announcements from "@/src/components/Announcements";
+import BigCalendar from "@/src/components/BigCalendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 const TeacherPage = () => {
-  return <div className="">TeacherPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT  */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-bold text-[#083765]">
+            Schedule ( 2ኛ ዓመት )
+          </h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* RIGHT  */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        {/* <EventCalendar /> */}
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
