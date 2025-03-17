4b9fcc58fec5b1bf09117bf0db491dbe657d97ad
Yakob Dereje <yakobdereje.yd@gmail.com>
2025-03-16 18:31:26 -0700

    Single Teacher Display Started

---
+++ course/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,13 @@
+const SingleTeacherPage = () => {
+  return (
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      {/* LEFT  */}
+      <div className="w-full xl:w-2/3">left side</div>
+
+      {/* RIGHT  */}
+      <div className="w-full xl:w-1/3">right side</div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
